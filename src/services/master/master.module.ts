c0d3d5225752713263c4110906cbe49aa373f92b
Sarun Daunghirun <sdsarun@outlook.com>
2025-03-22 23:25:14 +0700

    fix(master): missing inject provided and exported

--- src/services/master/master.module.ts
+++ src/services/master/master.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { MasterUserRole } from '../../database/models/master-user-role.model';
 import { MasterConversationType } from '../../database/models/master-conversation-type.model';
+import { MasterService } from './master.service';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([MasterUserRole, MasterConversationType]),
   ],
-  providers: []
+  providers: [MasterService],
+  exports: [MasterService, SequelizeModule]
 })
 export class MasterModule {}
