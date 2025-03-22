556c27607becd0237a0aaa3f3e07c1f3f19512f5
Sarun Daunghirun <sdsarun@outlook.com>
2025-03-22 22:54:59 +0700

    fix(infra): separate logic to handle graphql and http for excpetion filter, logger, format response

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -1,20 +1,39 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ContextType, ExceptionFilter } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import {
   getExceptionHttpStatus,
   getExceptionMessage,
 } from '../../shared/utils/exception.utils';
+import { Request, Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const type: ContextType | 'graphql' = host.getType();
+    switch (type) {
+      case 'graphql': {
+        return exception;
+      }
+
+      case 'http': {
+        this.handleHttpContext(exception, host);
+        break;
+      }
+
+      default: {
+        return exception;
+      }
+    }
+  }
+
+  private handleHttpContext(exception: any, host: ArgumentsHost) {
     const { httpAdapter } = this.httpAdapterHost;
 
     const ctx = host.switchToHttp();
-    const request = ctx.getRequest();
-    const response = ctx.getResponse();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
 
     const httpStatus: number = getExceptionHttpStatus(exception);
     const message: string = getExceptionMessage(exception);
