40d20ce0e226809eeee59948454b31f6919264b0
Sarun Daunghirun <sdsarun@outlook.com>
2025-03-23 13:17:40 +0700

    fix(auth): change search by username, aka match exactly

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { User as UserModel } from '../../database/models/user.model';
-import { User } from '../graphql/models/user.model';
-import { GetUserArgs } from './dto/args/get-user.args';
 import { WhereOptions } from 'sequelize';
-import { Op } from 'sequelize';
+import { User as UserModel } from '../../database/models/user.model';
 import { isDeepEmpty } from '../../shared/utils/validation/common.validation';
-import { CreateUserIfNotExistsInput } from './dto/input/create-user-if-not-exists.input';
+import { User } from '../graphql/models/user.model';
 import { MasterService } from '../master/master.service';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { CreateUserIfNotExistsInput } from './dto/input/create-user-if-not-exists.input';
 
 @Injectable()
 export class UserService {
@@ -28,15 +27,11 @@ export class UserService {
     }
 
     if (payload?.aka) {
-      where.aka = {
-        [Op.like]: `%${payload.aka.toLowerCase()}%`,
-      };
+      where.aka = payload.aka.toLowerCase();
     }
 
     if (payload?.username) {
-      where.aka = {
-        [Op.like]: `%${payload.username.toLowerCase()}%`,
-      };
+      where.username = payload.username.toLowerCase();
     }
 
     const user = await this.user.findOne({
@@ -57,8 +52,8 @@ export class UserService {
 
     const [userCreated] = await this.user.upsert(
       {
-        username: payload.username,
-        aka: payload?.aka || payload.username,
+        username: payload.username.toLowerCase(),
+        aka: payload?.aka?.toLowerCase() || payload.username.toLowerCase(),
         userRoleId: identifyRole.id,
       },
       { returning: true },
