d64f138e91761799f8dc69e9a88ddabfe3d083be
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-17 21:22:37 +0700

    chore(app): remove test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { User } from './database/models/user.model';
 
 @Controller()
 export class AppController {
@@ -9,15 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get("/test")
-  async getTest() {
-    User.create({
-      username: "sdfsdfsdf" + Math.random().toFixed(3)
-    })
-    return {
-      users: await User.findAll(),
-    }
   }
 }
