2f63ce12765d4bfa66cd1d5994d5db4e68576447
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-20 19:16:09 +0700

    fix(graphql): add omit apollo server landing page when not in development

--- src/services/graphql/graphql.module.ts
+++ src/services/graphql/graphql.module.ts
@@ -3,15 +3,28 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ConfigurationService } from '../../configuration/configuration.service';
+import { ApolloServerPlugin } from '@apollo/server';
 
 @Module({
   imports: [
-    NestGraphQLModule.forRoot<ApolloDriverConfig>({
+    NestGraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      autoSchemaFile: join(process.cwd(), 'src/services/graphql/schema.gql'),
+      inject: [ConfigurationService],
+      useFactory: (configurationService: ConfigurationService) => {
+        const plugins: ApolloServerPlugin[] = [];
+
+        if (configurationService.isDevelopment) {
+          plugins.push(ApolloServerPluginLandingPageLocalDefault());
+        }
+
+        return {
+          plugins,
+          playground: false,
+          autoSchemaFile: join(process.cwd(), 'src/services/graphql/schema.gql'),
+        }
+      }
     }),
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule { }
